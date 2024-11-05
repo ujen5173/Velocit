830d753b1e3f72f9427298856bebd30f37d225e6
Ujen Basi <ujenbasi12@gmail.com>
2024-11-06 01:49:38 +0545

    ...

--- src/app/(normal-user)/(landing-page)/page.tsx
+++ src/app/(normal-user)/(landing-page)/page.tsx
@@ -1,11 +1,14 @@
+import HeroSection from "~/app/_components/_/HeroSection";
+import ShopsAround from "~/app/_components/_/ShopsAround";
+import UpcomingEvent from "~/app/_components/_/UpcomingEvent";
+
 const Home = () => {
   return (
     <>
-      home
-      {/* <HeroSection /> */}
-      {/* <ShopsAround /> */}
+      <HeroSection />
+      <ShopsAround />
       {/* <Shops /> */}
-      {/* <UpcomingEvent /> */}
+      <UpcomingEvent />
     </>
   );
 };
